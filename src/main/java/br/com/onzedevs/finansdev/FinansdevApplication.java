7cf09a5c5eeb2ba41b846cd18a5e715946137b72
Montival <montival_junior@yahoo.com.br>
2020-07-31 22:18:46 -0300

    Primeiro commit

---
+++ src/main/java/br/com/onzedevs/finansdev/FinansdevApplication.java
@@ -0,0 +1,13 @@
+package br.com.onzedevs.finansdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinansdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinansdevApplication.class, args);
+	}
+
+}
